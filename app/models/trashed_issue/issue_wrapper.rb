dc1b655c75f2451c292a24fae8920b50b4be9275
maimai77 <kohta0707@gmail.com>
2021-08-27 17:20:25 +0900

    use DelegateClass

--- app/models/trashed_issue/issue_wrapper.rb
+++ app/models/trashed_issue/issue_wrapper.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 
 class TrashedIssue
-  class IssueWrapper
-    attr_reader :object, :child_ids, :relations
+  class IssueWrapper < DelegateClass(Issue)
+    attr_reader :child_ids, :relations
 
     def initialize(attributes)
-      @object = Issue.new(attributes.except('custom_field_values', 'child_ids', 'relations', 'journals',
-                                            'watcher_user_ids'))
-      @object.trashed = true
-      @object.priority_id = attributes['priority_id']
-      @object.custom_field_values = attributes['custom_field_values'] || {}
-      @object.watcher_users = User.where(id: attributes['watcher_user_ids'])
+      super(Issue.new(attributes.except(
+                        'custom_field_values',
+                        'child_ids',
+                        'relations',
+                        'journals',
+                        'watcher_user_ids'
+                      )))
+      self.trashed = true
+      self.priority_id = attributes['priority_id']
+      self.custom_field_values = attributes['custom_field_values'] || {}
+      self.watcher_users = User.where(id: attributes['watcher_user_ids'])
       @child_ids = attributes['child_ids'] || []
       @relations = (attributes['relations'] || []).map { |attr| IssueRelation.new(attr) }
       build_jornals(attributes)
@@ -33,28 +38,16 @@ class TrashedIssue
     end
 
     def assigned_to
-      return nil if @object.assigned_to_id.blank?
+      return nil if assigned_to_id.blank?
 
       super || User.anonymous
     end
 
     def fixed_version
-      return nil if @object.fixed_version_id.blank?
+      return nil if fixed_version_id.blank?
 
-      super || Version.new(id: @object.fixed_version_id, project: project,
+      super || Version.new(id: fixed_version_id, project: project,
                            name: "#{I18n.t(:missing_record_prefix)} #{I18n.t(:field_version)}")
-    end
-
-    def method_missing(name, *args)
-      if @object.respond_to?(name)
-        @object.send(name, *args)
-      else
-        super
-      end
-    end
-
-    def respond_to_missing?(symbol, _include_private)
-      !@object.respond_to?(symbol)
     end
 
     private
@@ -62,7 +55,7 @@ class TrashedIssue
     def build_jornals(attributes)
       return if attributes['journals'].blank?
 
-      @object.journals = attributes['journals'].map do |journal_attributes|
+      self.journals = attributes['journals'].map do |journal_attributes|
         Journal.new(journal_attributes.except('details')).tap do |journal|
           journal.details = journal_attributes['details'].map do |detail_attributes|
             JournalDetail.new(detail_attributes)
@@ -70,7 +63,7 @@ class TrashedIssue
         end
       end
 
-      @object.journals.each do |journal|
+      journals.each do |journal|
         journal.singleton_class.class_eval do
           def visible_details(user = User.current)
             details.select do |detail|
