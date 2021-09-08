41e8ce16b4c2be509c99ce0738f9616b00b03cb5
maimai77 <kohta0707@gmail.com>
2021-09-08 17:42:51 +0900

    add request spec

---
+++ spec/requests/issue_restoration_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require File.expand_path('../rails_helper', __dir__)
+
+RSpec.describe 'Issue restoration', type: :request do
+  subject { post '/restored_issues', params: { id: trashed.id } }
+  let!(:trashed) do
+    create(:issue).destroy
+    TrashedIssue.last
+  end
+
+  context 'when successfully restored' do
+    it { expect { subject }.to change(Issue, :count).by(1) }
+    it { expect { subject }.to change(TrashedIssue, :count).by(-1) }
+    it 'redirects to the restored issue' do
+      subject
+      expect(response).to redirect_to assigns(:issue)
+      expect(flash[:notice]).to eq 'Issue was successfully restored.'
+    end
+  end
+
+  context 'when restoration failed' do
+    before :each do
+      allow_any_instance_of(Issue).to receive(:subject).and_return nil
+    end
+    it { expect { subject }.to_not change(Issue, :count) }
+    it { expect { subject }.to_not change(TrashedIssue, :count) }
+    it 'redirects to the trashed issue' do
+      subject
+      expect(response).to redirect_to assigns(:trashed)
+      expect(flash[:error]).to eq 'Subject cannot be blank'
+    end
+  end
+end
