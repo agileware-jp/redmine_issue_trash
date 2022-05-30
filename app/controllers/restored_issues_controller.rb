d86e95973db03a4c3eaf5c49c0a58ff4aa192423
maimai77 <kohta0707@gmail.com>
2022-05-30 17:47:58 +0900

    support for Redmine5

--- app/controllers/restored_issues_controller.rb
+++ app/controllers/restored_issues_controller.rb
@@ -4,11 +4,14 @@ class RestoredIssuesController < ApplicationController
   def create
     @trashed = TrashedIssue.find(params[:id])
     @issue = @trashed.rebuild
+    @issue.watcher_users = []
     @issue.init_journal(User.current, t(:notes_issue_restored))
 
     begin
       ActiveRecord::Base.transaction do
         @issue.save!
+        # avoid conflict with Journal#add_watcher in Redmine5 or later
+        @issue.watcher_users = User.where(id: @issue.watcher_user_ids | @trashed.attributes_json['watcher_user_ids'])
         @trashed.destroy!
       end
       redirect_to @issue, notice: t(:notice_issue_restored)
